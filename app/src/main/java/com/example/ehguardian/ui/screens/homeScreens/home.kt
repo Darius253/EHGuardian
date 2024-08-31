9ca3e26a9b5fb15bc93f8a156a4c3b6a88a085bc
Darius253 <trondarius13@gmail.com>
2024-08-31 02:16:21 +0100

    Added Navigation

--- app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/home.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/home.kt
@@ -1,2 +1,212 @@
 package com.example.ehguardian.ui.screens.homeScreens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.ehguardian.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.remember
+import java.time.LocalTime
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Home(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 10.dp)
+            .fillMaxSize()
+    ) {
+        item { Header() }
+        item { Spacer(modifier = Modifier.height(10.dp)) }
+        item {
+            Text(
+                text = "Here is your last health recorded data",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        item {
+            HealthCard(
+                title = "Blood Pressure",
+                imageRes = R.drawable.arm,
+                backgroundColor = MaterialTheme.colorScheme.primaryContainer,
+                textColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextData("Systolic", "120", MaterialTheme.colorScheme.onPrimaryContainer)
+                    TextData("Diastolic", "80", MaterialTheme.colorScheme.onPrimaryContainer)
+                    TextData("Pulse", "80", MaterialTheme.colorScheme.onPrimaryContainer)
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        item {
+            HealthCard(
+                title = "Heart Rate",
+                imageRes = R.drawable.blood_pressure,
+                backgroundColor = MaterialTheme.colorScheme.secondaryContainer,
+                textColor = MaterialTheme.colorScheme.onSecondaryContainer
+            ) {
+                TextData(
+                    text = "bpm",
+                    value = "82",
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    modifier = Modifier.padding(start = 60.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        item {
+            HealthCard(
+                title = "Body Mass Index",
+                imageRes = R.drawable.heart_rate_monitor,
+                backgroundColor = MaterialTheme.colorScheme.tertiaryContainer,
+                textColor = MaterialTheme.colorScheme.onTertiaryContainer
+            ) {
+                TextData(
+                    text = "kg/m²",
+                    value = "24",
+                    color = MaterialTheme.colorScheme.onTertiaryContainer,
+                    modifier = Modifier.padding(start = 60.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Header(modifier: Modifier = Modifier) {
+    val currentHour = remember { LocalTime.now().hour }
+    val greeting = when (currentHour) {
+        in 5..11 -> "Good Morning,"
+        in 12..17 -> "Good Afternoon,"
+        in 18..21 -> "Good Evening,"
+        else -> "Good Night,"
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(greeting, style = MaterialTheme.typography.headlineSmall)
+            Text(
+                text = "Darius Twumasi-Ankrah",
+                style = MaterialTheme.typography.headlineSmall,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+        }
+
+        IconButton(onClick = { /* Handle settings icon click */ }) {
+            Icon(
+                imageVector = Icons.Outlined.Settings,
+                contentDescription = "Settings",
+                modifier = Modifier.size(32.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun HealthCard(
+    title: String,
+    imageRes: Int,
+    backgroundColor: Color,
+    textColor: Color,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .background(backgroundColor)
+            .height(200.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(25.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = "$title Image",
+                    modifier = Modifier.size(48.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold,
+                    color = textColor
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            content()
+        }
+    }
+}
+
+@Composable
+fun TextData(
+    text: String,
+    value: String,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = value,
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+            color = color
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Normal,
+            color = color
+        )
+    }
+}
