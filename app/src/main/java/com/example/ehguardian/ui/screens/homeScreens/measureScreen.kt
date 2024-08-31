8cb761c03f70f78456838ed5521dc1d8104d9e1b
Darius253 <trondarius13@gmail.com>
2024-08-31 02:15:03 +0100

    Added Navigation

--- app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/measureScreen.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/measureScreen.kt
@@ -1,2 +1,14 @@
 package com.example.ehguardian.ui.screens.homeScreens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun MeasureScreen(modifier: androidx.compose.ui.Modifier){
+
+    Text(text = "Measure Screen")
+
+
+}
+
