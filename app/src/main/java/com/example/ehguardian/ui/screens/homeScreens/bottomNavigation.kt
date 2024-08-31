8b805723c2bbc044d89b161dcd3b4a00d6ec2f81
Darius253 <trondarius13@gmail.com>
2024-08-31 02:14:53 +0100

    Added Navigation

--- app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/bottomNavigation.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/bottomNavigation.kt
@@ -1,2 +1,101 @@
 package com.example.ehguardian.ui.screens.homeScreens
 
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BottomNavigation(
+    items: List<BottomNavItem>,
+    selectedItem: Int,
+    onItemSelected: (Int) -> Unit
+) {
+    NavigationBar(
+        modifier = Modifier
+            .height(70.dp)
+            .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+    ) {
+        items.forEachIndexed { index, item ->
+            AddItem(
+                item = item,
+                isSelected = index == selectedItem,
+                onClick = { onItemSelected(index) }
+            )
+        }
+    }
+}
+
+sealed class BottomNavItem(
+    val title: String,
+    val icon: ImageVector,
+    val selectedIcon: ImageVector
+) {
+    data object Home : BottomNavItem(
+        title = "Home",
+        icon = Icons.Outlined.Home,
+        selectedIcon = Icons.Filled.Home
+    )
+
+    data object Health : BottomNavItem(
+        title = "Measure",
+        icon = Icons.Outlined.MonitorHeart,
+        selectedIcon = Icons.Filled.MonitorHeart
+    )
+
+    data object History : BottomNavItem(
+        title = "History",
+        icon = Icons.Outlined.StackedBarChart,
+        selectedIcon = Icons.Filled.StackedBarChart
+    )
+
+    object Profile : BottomNavItem(
+        title = "Profile",
+        icon = Icons.Outlined.Person,
+        selectedIcon = Icons.Filled.Person
+    )
+}
+
+@Composable
+fun RowScope.AddItem(
+    item: BottomNavItem,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    NavigationBarItem(
+        modifier = Modifier.padding(top = 26.dp),
+        label = {
+            Text(
+                text = item.title,
+                style = MaterialTheme.typography.labelMedium,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        icon = {
+            Icon(
+                imageVector = if (isSelected) item.selectedIcon else item.icon,
+                contentDescription = item.title
+            )
+        },
+        selected = isSelected,
+        alwaysShowLabel = true,
+        onClick = onClick,
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = MaterialTheme.colorScheme.primary,
+            unselectedIconColor = MaterialTheme.colorScheme.onSurface,
+            selectedTextColor = MaterialTheme.colorScheme.primary,
+            unselectedTextColor = MaterialTheme.colorScheme.onSurface,
+            indicatorColor = MaterialTheme.colorScheme.surface
+        )
+    )
+}
