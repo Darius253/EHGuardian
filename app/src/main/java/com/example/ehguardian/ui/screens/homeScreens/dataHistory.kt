4d45fe0141bcd3a9eacaf974dfb91823338d2d48
Darius253 <trondarius13@gmail.com>
2024-08-31 02:13:34 +0100

    Worked on Page

---
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/dataHistory.kt
@@ -0,0 +1,12 @@
+package com.example.ehguardian.ui.screens.homeScreens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun DataHistoryScreen(modifier: Modifier){
+    Text(text = "Data History")
+
+}
---
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/settings.kt
@@ -0,0 +1,2 @@
+package com.example.ehguardian.ui.screens.homeScreens
+
