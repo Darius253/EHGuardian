1578592e68f2e04d2be632bb968d2f04b44faece
Darius253 <trondarius13@gmail.com>
2024-08-31 02:15:40 +0100

    Added Navigation

--- app/src/main/java/com/example/ehguardian/ui/Navigation.kt
+++ app/src/main/java/com/example/ehguardian/ui/Navigation.kt
@@ -1,10 +1,47 @@
 package com.example.ehguardian.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.ehguardian.ui.screens.authenticationScreens.AuthenticationScreen
+import com.example.ehguardian.ui.screens.homeScreens.HomeScreen
 
+// Enum class to define the different navigation destinations
+enum class NavigationClass(val route: String) {
+    AuthenticationDestination(route = "authentication"),
+    HomeDestination(route = "home"),
+    ProfileDestination(route = "profile"),
+}
 
 @Composable
-fun Navigation(){
-
-
+fun AppNavigation(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavigationClass.AuthenticationDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = NavigationClass.AuthenticationDestination.route) {
+            AuthenticationScreen(
+                modifier = modifier,
+                onSignInClick = {
+                    navController.navigate(NavigationClass.HomeDestination.route)
+                },
+                onSignUpClick = {
+                        navController.navigate(NavigationClass.HomeDestination.route)
+                },
+                onForgotPasswordClick = {})
+        }
+        composable(route = NavigationClass.HomeDestination.route) {
+            // Home Screen
+            HomeScreen()
+        }
+        composable(route = NavigationClass.ProfileDestination.route) {
+            // Profile Screen
+        }
+    }
 }
