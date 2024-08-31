b14f3204ba2e4a56fa7d9f3da6f2a9d4bb54f034
Darius253 <trondarius13@gmail.com>
2024-08-31 02:15:55 +0100

    Added Navigation

--- app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/settings.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/settings.kt
@@ -1,2 +1,10 @@
 package com.example.ehguardian.ui.screens.homeScreens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+
+fun Settings(){
+    Text(text = "Settings")
+}
