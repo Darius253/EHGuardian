c58206ed7e284988b786cf628d98430cd39125a4
Darius253 <trondarius13@gmail.com>
2024-08-31 02:14:42 +0100

    Added Navigation

--- app/src/main/java/com/example/ehguardian/ui/screens/myApp.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/myApp.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.ehguardian.ui.screens.authenticationScreens.AuthenticationScreen
-
+import androidx.navigation.compose.rememberNavController
+import com.example.ehguardian.ui.AppNavigation
 
 @Composable
 fun MyApp() {
+    // Remembering the NavController for handling navigation
+    val navController = rememberNavController()
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-        AuthenticationScreen(modifier = Modifier.padding(innerPadding))
+        // Passing the NavController and modifier with padding to the AppNavigation
+        AppNavigation(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
