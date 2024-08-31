569dda9a3ac1f1bba888934562bfa2aeb9d22ad9
Darius253 <trondarius13@gmail.com>
2024-08-31 02:16:07 +0100

    Added Navigation

--- app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/profileScreen.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/profileScreen.kt
@@ -1,2 +1,12 @@
 package com.example.ehguardian.ui.screens.homeScreens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun ProfileScreen(modifier: Modifier) {
+
+    Text(text = "Profile Screen")
+}
