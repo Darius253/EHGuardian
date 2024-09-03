f0f3388d17a5e86cf3393c634f12e387b9d8e6b8
Darius253 <trondarius13@gmail.com>
2024-09-03 01:32:32 +0100

    Worked on Settings Popup and Measure Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.6.8")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation ("com.github.fracassi-marco:JetChart:1.4.1")
+    implementation("androidx.bluetooth:bluetooth:1.0.0-alpha02")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
