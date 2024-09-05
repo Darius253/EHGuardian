60dc758fda826deb6e0eac43bec1c9e1e1920c3e
Darius253 <trondarius13@gmail.com>
2024-09-05 23:46:17 +0100

    Added viewModel and Firebase

--- app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/measureScreen/measureScreen.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/measureScreen/measureScreen.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.ehguardian.ui.screens.homeScreens.measureScreen
 
 import android.Manifest
@@ -5,23 +7,13 @@ import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import android.content.pm.PackageManager
-import android.os.Build
 import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -29,32 +21,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
-import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.compose.*
 import com.example.ehguardian.R
-import com.example.ehguardian.ui.screens.homeScreens.profile.InputField
-import kotlinx.coroutines.launch
 
-@RequiresApi(Build.VERSION_CODES.S)
-@SuppressLint("CoroutineCreationDuringComposition")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MeasureScreen(modifier: Modifier = Modifier) {
-
     val (showDialog, setShowDialog) = rememberSaveable { mutableStateOf(false) }
     val (systolic, setSystolic) = rememberSaveable { mutableStateOf("") }
     val (diastolic, setDiastolic) = rememberSaveable { mutableStateOf("") }
     val (heartRate, setHeartRate) = rememberSaveable { mutableStateOf("") }
-    var (bluetoothEnabled, setBluetoothEnabled) = rememberSaveable { mutableStateOf(false) }
-
-
+    val (bluetoothEnabled, setBluetoothEnabled) = rememberSaveable { mutableStateOf(false) }
 
 
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.bluetooth))
@@ -64,21 +45,15 @@ fun MeasureScreen(modifier: Modifier = Modifier) {
     )
 
     val sheetState = rememberModalBottomSheetState()
-    val coroutineScope = rememberCoroutineScope()
 
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        if (!bluetoothEnabled ) {
-            ViewBluetoothDevices(
-                onClick = {
-
-                    setBluetoothEnabled(true)
-
-            }
+        if (!bluetoothEnabled) {
+            ViewBluetoothDevicesButton(
+                onClick = { setBluetoothEnabled(true) }
             )
         } else {
             BluetoothAnimation(composition = composition, progress = progress)
-//            coroutineScope.launch { sheetState.show() }
-            ViewBluetoothDevices(onDismiss = { setBluetoothEnabled(false) }, sheetState = sheetState)
+            ViewBluetoothDevicesSheet(onDismiss = { setBluetoothEnabled(false) }, sheetState = sheetState)
         }
 
         AddDetailsFab { setShowDialog(true) }
@@ -98,23 +73,30 @@ fun MeasureScreen(modifier: Modifier = Modifier) {
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.S)
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("InlinedApi")
 @Composable
-fun ViewBluetoothDevices(onDismiss: () -> Unit, sheetState: SheetState) {
+fun ViewBluetoothDevicesSheet(onDismiss: () -> Unit, sheetState: SheetState) {
     val context = LocalContext.current
-    val bluetoothManager = getSystemService(context, BluetoothManager::class.java)
+    val bluetoothManager = context.getSystemService(BluetoothManager::class.java)
     val bluetoothAdapter: BluetoothAdapter? = bluetoothManager?.adapter
     val foundDevices = remember { mutableStateListOf<BluetoothDevice>() }
 
+
     LaunchedEffect(Unit) {
-        val hasPermission = ContextCompat.checkSelfPermission(
+        val hasPermissionScan = ContextCompat.checkSelfPermission(
             context, Manifest.permission.BLUETOOTH_SCAN
         ) == PackageManager.PERMISSION_GRANTED
 
-        if (hasPermission) {
+        val hasPermissionBluetooth = ContextCompat.checkSelfPermission(
+            context, Manifest.permission.BLUETOOTH
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (hasPermissionScan || hasPermissionBluetooth) {
             bluetoothAdapter?.startDiscovery()
+            Toast.makeText(context, "Searching for devices", Toast.LENGTH_SHORT).show()
         } else {
+
             Toast.makeText(context, "Bluetooth permission not granted", Toast.LENGTH_SHORT).show()
         }
     }
@@ -124,7 +106,7 @@ fun ViewBluetoothDevices(onDismiss: () -> Unit, sheetState: SheetState) {
             override fun onReceive(context: Context, intent: Intent) {
                 when (intent.action) {
                     BluetoothDevice.ACTION_FOUND -> {
-                        val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                        val device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
                         if (device != null && !foundDevices.contains(device)) {
                             foundDevices.add(device)
                         }
@@ -146,8 +128,7 @@ fun ViewBluetoothDevices(onDismiss: () -> Unit, sheetState: SheetState) {
         sheetState = sheetState,
         onDismissRequest = onDismiss,
         shape = RoundedCornerShape(12.dp),
-        )
-        {
+    ) {
         Column(
             modifier = Modifier
                 .padding(16.dp)
@@ -165,7 +146,9 @@ fun ViewBluetoothDevices(onDismiss: () -> Unit, sheetState: SheetState) {
             if (foundDevices.isNotEmpty()) {
                 LazyColumn {
                     items(foundDevices) { device ->
-                        BluetoothDeviceItem(device = device)
+                        BluetoothDeviceItem(device = device) {
+                            connectToDevice(context, device)
+                        }
                     }
                 }
             } else {
@@ -175,243 +158,56 @@ fun ViewBluetoothDevices(onDismiss: () -> Unit, sheetState: SheetState) {
     }
 }
 
-
-
-
-
-
 @Composable
-fun BluetoothDeviceItem(device: BluetoothDevice) {
+fun BluetoothDeviceItem(device: BluetoothDevice, onConnect: (BluetoothDevice) -> Unit) {
     val context = LocalContext.current
-    Column(modifier = Modifier.padding(vertical = 8.dp)) {
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.BLUETOOTH_CONNECT
-            ) != PackageManager.PERMISSION_GRANTED
+    var deviceName by remember { mutableStateOf("Unnamed Device") }
+
+    // Check if BLUETOOTH_CONNECT permission is granted
+    if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT)
+        == PackageManager.PERMISSION_GRANTED ||
+        ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED
+    ) {
+        deviceName = device.name ?: "Unnamed Device"
+    }
+
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
         ) {
-            // Handle permission request
-            return
+            Column {
+                Text(
+                    text = deviceName,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(text = device.address, style = MaterialTheme.typography.bodySmall)
+            }
+            Button(onClick = { onConnect(device) }) {
+                Text("Connect")
+            }
         }
-        Text(
-            text = device.name ?: "Unnamed Device",
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.titleMedium
-        )
-        Text(
-            text = device.address,
-            style = MaterialTheme.typography.bodySmall
-        )
-        HorizontalDivider(color = Color.Gray)
+        Divider(color = Color.Gray)
     }
 }
 
 @Composable
-fun ViewBluetoothDevices(onClick:  () -> Unit) {
+fun ViewBluetoothDevicesButton(onClick: () -> Unit) {
     Button(onClick = onClick) {
         Text(text = "View Available Bluetooth Devices")
     }
 }
 
-@Composable
-fun BluetoothAnimation(composition: LottieComposition?, progress: Float) {
-    LottieAnimation(
-        composition = composition,
-        progress = progress,
-        modifier = Modifier.padding(16.dp)
-    )
-}
-
-@Composable
-fun AddDetailsFab(onClick: () -> Unit) {
-    FloatingActionButton(
-        onClick = onClick,
-        modifier = Modifier.padding(top = 500.dp, start = 300.dp)
+private fun connectToDevice(context: Context, device: BluetoothDevice) {
+    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT)
+        == PackageManager.PERMISSION_GRANTED
     ) {
-        Icon(imageVector = Icons.Default.Add, contentDescription = "Add Details Manually")
-    }
-}
-
-@Composable
-fun OverlayBackground() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black.copy(alpha = 0.5f))
-    )
-}
-
-@Composable
-fun ManuallyAddDetails(
-    onDismiss: () -> Unit,
-    systolic: String,
-    diastolic: String,
-    heartRate: String,
-    onSystolicChange: (String) -> Unit,
-    onDiastolicChange: (String) -> Unit,
-    onHeartRateChange: (String) -> Unit
-) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.8f)
-            .padding(10.dp),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center
-        ) {
-            DialogHeader(onDismiss = onDismiss)
-            Spacer(modifier = Modifier.height(16.dp))
-            ExpandableSection(
-                title = "Blood Pressure",
-                iconResId = R.drawable.arm
-            ) {
-                Spacer(modifier = Modifier.height(16.dp))
-                BloodPressureInputs(
-                    systolic = systolic,
-                    diastolic = diastolic,
-                    onSystolicChange = onSystolicChange,
-                    onDiastolicChange = onDiastolicChange
-                )
-            }
-            Spacer(modifier = Modifier.height(25.dp))
-            ExpandableSection(
-                title = "Heart Rate",
-                iconResId = R.drawable.heart_rate_monitor
-            ) {
-                Spacer(modifier = Modifier.height(16.dp))
-                HeartRateInput(
-                    heartRate = heartRate,
-                    onHeartRateChange = onHeartRateChange
-                )
-            }
-            Spacer(modifier = Modifier.height(20.dp))
-            UploadButton(
-                onUpload = { onDismiss() },
-                systolic = systolic,
-                diastolic = diastolic,
-                heartRate = heartRate
-            )
-        }
-    }
-}
-
-@Composable
-fun DialogHeader(onDismiss: () -> Unit) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "Manually Input Data",
-            style = MaterialTheme.typography.titleLarge,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        IconButton(onClick = onDismiss) {
-            Icon(
-                imageVector = Icons.Default.Close,
-                contentDescription = "Close",
-                tint = Color.Red
-            )
-        }
-    }
-}
-
-@Composable
-fun ExpandableSection(
-    title: String,
-    iconResId: Int,
-    content: @Composable () -> Unit
-) {
-    Column {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                modifier = Modifier.size(48.dp),
-                painter = painterResource(id = iconResId),
-                contentDescription = "$title Image"
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold
-            )
-        }
-        content()
-    }
-}
-
-@Composable
-fun BloodPressureInputs(
-    systolic: String,
-    diastolic: String,
-    onSystolicChange: (String) -> Unit,
-    onDiastolicChange: (String) -> Unit
-) {
-    Row {
-        InputField(
-            label = "Systolic",
-            value = systolic,
-            onValueChange = onSystolicChange,
-            suffix = "mmHg",
-            modifier = Modifier.weight(1f),
-            keyboardType = KeyboardType.Number
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        InputField(
-            label = "Diastolic",
-            value = diastolic,
-            onValueChange = onDiastolicChange,
-            suffix = "mmHg",
-            modifier = Modifier.weight(1f),
-            keyboardType = KeyboardType.Number
-        )
-    }
-}
-
-@Composable
-fun HeartRateInput(
-    heartRate: String,
-    onHeartRateChange: (String) -> Unit
-) {
-    InputField(
-        label = "Pulse",
-        value = heartRate,
-        onValueChange = onHeartRateChange,
-        suffix = "bpm",
-        modifier = Modifier.fillMaxWidth(),
-        keyboardType = KeyboardType.Number
-    )
-}
-
-@Composable
-fun UploadButton(
-    onUpload: () -> Unit,
-    systolic: String,
-    diastolic: String,
-    heartRate: String
-) {
-    val context = LocalContext.current
-
-    TextButton(
-        modifier = Modifier.padding(start = 200.dp),
-        onClick = {
-            onUpload()
-            Toast.makeText(context, "$systolic $diastolic $heartRate", Toast.LENGTH_SHORT).show()
-        }
-    ) {
-        Text(
-            text = "Upload",
-            style = MaterialTheme.typography.titleMedium,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary
-        )
+        device.createBond() // Trigger Bluetooth device pairing
+        Toast.makeText(context, "Attempting to pair with ${device.name}", Toast.LENGTH_SHORT).show()
+    } else {
+        Toast.makeText(context, "Bluetooth connect permission not granted", Toast.LENGTH_SHORT).show()
     }
 }
