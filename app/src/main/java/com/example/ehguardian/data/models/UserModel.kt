bea307df87381568f1e2bc3da7a18fd0975584e3
Darius253 <trondarius13@gmail.com>
2024-09-05 23:47:07 +0100

    Added viewModel and Firebase

--- app/src/main/java/com/example/ehguardian/data/models/UserModel.kt
+++ app/src/main/java/com/example/ehguardian/data/models/UserModel.kt
@@ -1,3 +1,18 @@
 package com.example.ehguardian.data.models
 
-data class UserModel()
+
+data class UserModel(
+    var id: Int,
+    var name: String,
+    var email: String,
+    var gender: String,
+    var status: String,
+    var createdAt: String,
+    var password: String,
+    var firstName: String,
+    var lastName: String,
+    var userWeight: String,
+    var userHeight: String,
+    var dateOfBirth: String,
+    var measurementData: MeasurementData,
+    )
