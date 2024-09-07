79077d8f1283fb03468eea38fec243312bb16992
Darius253 <trondarius13@gmail.com>
2024-09-07 14:40:24 +0100

    Worked on Authentication

--- app/src/main/java/com/example/ehguardian/EhGuardianApplication.kt
+++ app/src/main/java/com/example/ehguardian/EhGuardianApplication.kt
@@ -1,4 +1,40 @@
 package com.example.ehguardian
 
-class EhGuardianApplication {
+import android.app.Application
+import com.example.ehguardian.data.repositories.FirebaseUserRepository
+import com.example.ehguardian.data.repositories.UserRepository
+import com.example.ehguardian.data.services.Authentication
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+class EhGuardianApplication : Application() {
+
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize Firebase
+        container = AppDataContainer()
+    }
+}
+
+interface AppContainer {
+    val userRepository: UserRepository
+}
+
+class AppDataContainer : AppContainer {
+    // Provide Firebase services
+    private val firebaseAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val firestore: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+
+    // Providing the concrete implementation of UserRepository
+    override val userRepository: UserRepository by lazy {
+        FirebaseUserRepository(
+            authService = Authentication(
+                auth = firebaseAuth,
+                firestore = firestore
+            ),
+
+        )
+    }
 }
