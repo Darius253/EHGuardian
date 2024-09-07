032bfaa30954e73b70a11dab46181b630867c2de
Darius253 <trondarius13@gmail.com>
2024-09-07 14:40:34 +0100

    Worked on Authentication

--- app/src/main/java/com/example/ehguardian/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/ehguardian/ui/AppViewModelProvider.kt
@@ -1,4 +1,29 @@
 package com.example.ehguardian.ui
 
-class AppViewModelProvider {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ehguardian.EhGuardianApplication
+import com.example.ehguardian.ui.screens.authenticationScreens.login.LoginViewModel
+import com.example.ehguardian.ui.screens.authenticationScreens.signUp.SignUpViewModel
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Access the application instance and its dependencies
+        initializer {
+            val app = (this as CreationExtras).ehGuardianApplication()
+            LoginViewModel(app.container.userRepository)
+        }
+        initializer {
+            val app = (this as CreationExtras).ehGuardianApplication()
+            SignUpViewModel(app.container.userRepository)
+        }
+    }
+}
+
+fun CreationExtras.ehGuardianApplication(): EhGuardianApplication {
+    return (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as? EhGuardianApplication)
+        ?: throw IllegalStateException("Application is not an instance of EhGuardianApplication")
 }
