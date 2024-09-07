f9302e37571213a81bb919bc9889fbddb23c2698
Darius253 <trondarius13@gmail.com>
2024-09-07 14:40:54 +0100

    Worked on Authentication

--- app/src/main/java/com/example/ehguardian/data/models/MeasureData.kt
+++ app/src/main/java/com/example/ehguardian/data/models/MeasureData.kt
@@ -1,12 +1,15 @@
 package com.example.ehguardian.data.models
 
 data class MeasurementData(
-    var id: Int,
-    var systolic: String,
-    var diastolic: String,
-    var heartRate: String,
-    var timestamp: String,
-    var userId: Int,
+    var id: Int = 0,
+    var systolic: String = "",
+    var diastolic: String ="",
+    var heartRate: String="",
+    var timestamp: String="",
+    var userId: Int=0,
+    var cholesterolLevel: String="",
+    var bloodSugarLevel: String="",
+    var dateStamp: String="",
 
 
 )
