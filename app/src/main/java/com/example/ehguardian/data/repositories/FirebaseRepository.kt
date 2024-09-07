8f1bdc75c89b4332e6ce496a76058127c8d4fff6
Darius253 <trondarius13@gmail.com>
2024-09-07 14:41:49 +0100

    Worked on Authentication

--- app/src/main/java/com/example/ehguardian/data/repositories/FirebaseRepository.kt
+++ app/src/main/java/com/example/ehguardian/data/repositories/FirebaseRepository.kt
@@ -1,27 +1,20 @@
 package com.example.ehguardian.data.repositories
 
-import android.app.Activity
-import android.content.Context
-import android.util.Log
-import android.widget.Toast
-import androidx.compose.ui.platform.LocalContext
+
 import com.example.ehguardian.data.models.MeasurementData
 import com.example.ehguardian.data.models.UserModel
 import com.example.ehguardian.data.services.Authentication
-import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 
 class FirebaseUserRepository(
     private val authService: Authentication,
-    private val firestore: FirebaseFirestore
+
 ) : UserRepository {
 
     override fun userSignIn(email: String, password: String, onComplete: (Boolean, String?) -> Unit) {
         authService.signIn(email, password) { success, userId ->
             if (success) {
-
         onComplete(true, userId)
-
 
             } else {
              onComplete(false, userId)
@@ -34,15 +27,18 @@ class FirebaseUserRepository(
     override fun userSignUp(user: UserModel, onComplete: (Boolean, String?) -> Unit) {
         authService.signUp(
             user,
-
         ) { success, userId ->
             if (success && userId != null) {
-                user.id = userId.toInt()
-                firestore.collection("users").document(userId).set(user)
+                onComplete(true, userId)
             } else {
                 onComplete(false, userId)
             }
         }
+    }
+
+    override suspend fun signOut(){
+        authService.signOut()
+
     }
 
     // Implement other methods of UserRepository
@@ -54,7 +50,7 @@ class FirebaseUserRepository(
 
 
     override suspend fun updateUserDetails(user: UserModel) = TODO()
-    override suspend fun signOut() = TODO()
+
     override suspend fun deleteAccount(user: UserModel) = TODO()
     override suspend fun uploadUserMeasurement(measurement: MeasurementData) = TODO()
     override fun getUserMeasurements(): Flow<List<MeasurementData>> = TODO()
