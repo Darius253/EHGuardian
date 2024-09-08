ae2c12eef40d2f4456f3c3f75379c69637df9a63
Darius253 <trondarius13@gmail.com>
2024-09-08 01:48:36 +0100

    Firebase Integration

--- app/src/main/java/com/example/ehguardian/data/repositories/UserRepository.kt
+++ app/src/main/java/com/example/ehguardian/data/repositories/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.example.ehguardian.data.repositories
 
+
 import com.example.ehguardian.data.models.MeasurementData
 import com.example.ehguardian.data.models.UserModel
 import kotlinx.coroutines.flow.Flow
@@ -10,18 +11,19 @@ interface  UserRepository{
 
     fun userSignUp(user:UserModel, onComplete: (Boolean, String?) -> Unit)
 
-    fun getUser(): Flow<UserModel>
+    suspend fun getUser(): Flow<UserModel>
 
-    suspend fun updateUserDetails(user: UserModel)
+    suspend fun updateUserDetails(user: UserModel):Boolean
 
     suspend fun signOut()
 
     suspend fun deleteAccount(user: UserModel)
 
-    suspend fun uploadUserMeasurement(measurement: MeasurementData)
+    suspend fun uploadUserMeasurement(measurementData: MeasurementData):Boolean
 
-    fun getUserMeasurements(): Flow<List<MeasurementData>>
 
-    suspend fun getUserLatestMeasurementByUserId(userId: Int): List<MeasurementData>
+    suspend fun getUserMeasurements(): Flow<List<MeasurementData>>
+
+    suspend fun getUserLatestMeasurementByUserId(userId: Int): Flow<List<MeasurementData>>
 
 }
