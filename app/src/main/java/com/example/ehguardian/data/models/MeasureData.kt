c0bfc4c33924fe934671e383583ec9a31891c64d
Darius253 <trondarius13@gmail.com>
2024-09-08 01:46:45 +0100

    Firebase Integration

--- app/src/main/java/com/example/ehguardian/data/models/MeasureData.kt
+++ app/src/main/java/com/example/ehguardian/data/models/MeasureData.kt
@@ -1,15 +1,10 @@
 package com.example.ehguardian.data.models
 
+
 data class MeasurementData(
-    var id: Int = 0,
-    var systolic: String = "",
-    var diastolic: String ="",
-    var heartRate: String="",
-    var timestamp: String="",
-    var userId: Int=0,
-    var cholesterolLevel: String="",
-    var bloodSugarLevel: String="",
-    var dateStamp: String="",
-
-
+    val systolic: String = "",
+    val diastolic: String = "",
+    val pulse: String = "",
+    val timestamp: String = "",
+    val bmi: String = ""
 )
