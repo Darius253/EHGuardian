7cf81b7219fc968d347168340dbb4e05edc7d0c0
Darius253 <trondarius13@gmail.com>
2024-09-11 18:41:13 +0100

    Worked on news api

--- app/src/main/java/com/example/ehguardian/network/HealthNewsApi.kt
+++ app/src/main/java/com/example/ehguardian/network/HealthNewsApi.kt
@@ -1,40 +1,50 @@
 package com.example.ehguardian.network
 
 import com.example.ehguardian.data.models.HealthNewsModel
-import retrofit2.Call
+import com.example.ehguardian.data.models.NewsRequest
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 
 // Base URL of your API
-private const val BASE_URL = "https://newsnow.p.rapidapi.com/"
+
 
 // Define your Retrofit API interface
 // Retrofit API Service
 interface NewsApiService {
-
     @Headers(
-        "x-rapidapi-key: YOUR_API_KEY",
+        "x-rapidapi-key: 03ef9b87e2mshcf6283f5332f6eep1504b5jsnd62b13d8370c",
         "x-rapidapi-host: newsnow.p.rapidapi.com",
         "Content-Type: application/json"
     )
     @POST("newsv2_top_news_cat")
-    fun getNews(@Body body: String): HealthNewsModel
+    suspend fun getNews(@Body body: NewsRequest): Response<HealthNewsModel>
 }
 
-// Retrofit instance with Gson converter
-private val retrofit = Retrofit.Builder()
-    .baseUrl(BASE_URL)
-    .addConverterFactory(GsonConverterFactory.create())  // Gson converter for JSON parsing
+
+
+
+private val okHttpClient = OkHttpClient.Builder()
+    .connectTimeout(2, TimeUnit.MINUTES)  // Connection timeout
+    .writeTimeout(2, TimeUnit.MINUTES)     // Write timeout
+    .readTimeout(2, TimeUnit.MINUTES)      // Read timeout
     .build()
 
+private val retrofit = Retrofit.Builder()
+    .baseUrl("https://newsnow.p.rapidapi.com/")
+    .client(okHttpClient)  // Set custom OkHttpClient
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
 
-// Singleton for accessing the API
 object NewsApi {
     val retrofitService: NewsApiService by lazy {
         retrofit.create(NewsApiService::class.java)
     }
 }
+
