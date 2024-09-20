8ce9fb1fb8d1a6f928cfd45f81d9fc9fb022ccff
Darius253 <trondarius13@gmail.com>
2024-09-20 18:52:51 +0100

    Working on maps page

--- app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/settings/NearbyHospitalsScreen.kt
+++ app/src/main/java/com/example/ehguardian/ui/screens/homeScreens/settings/NearbyHospitalsScreen.kt
@@ -1,2 +1,207 @@
 package com.example.ehguardian.ui.screens.homeScreens.settings
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Directions
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.ehguardian.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NearbyHospitals(onDismiss: () -> Unit) {
+    val items = listOf(
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    )
+
+    val sheetState = rememberModalBottomSheetState()
+    ModalBottomSheet(
+        containerColor = MaterialTheme.colorScheme.background,
+        onDismissRequest = onDismiss,
+        shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+        sheetState = sheetState,
+        dragHandle = { ModalBottomHeader(headerText = "Nearby Hospitals", onDismiss = onDismiss) }
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(0.95f),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        )
+        {
+            items(items.size) { item ->
+                LocationInfo()
+            }
+        }
+
+
+
+
+
+
+    }
+}
+
+
+
+@Composable
+fun LocationInfo(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        HospitalInfo(
+            name = "Name of Hospital",
+            address = "8 Hunter Close, Borehamwood",
+            rating = "2.8/5.0",
+            distance = "800m away",
+            status = "Closed"
+        )
+        ActionButtons()
+        HorizontalDivider(
+
+        )
+    }
+
+
+}
+
+
+@Composable
+fun HospitalInfo(
+    name: String,
+    address: String,
+    rating: String,
+    distance: String,
+    status: String
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(4.dp),) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Box(
+                modifier = Modifier.background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(12.dp)
+                ),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                    text = rating,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 0.dp),
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.map),
+                contentDescription = "Location Icon",
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = address,
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.W400
+            )
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.walk),
+                contentDescription = "Distance Icon",
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = distance)
+        }
+
+        Text(
+            text = status,
+            color = MaterialTheme.colorScheme.error
+        )
+    }
+}
+
+@Composable
+fun ActionButtons() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        ActionButtonItem(
+            onClick = { /*TODO*/ },
+            icon = Icons.Outlined.Phone,
+            text = "Call"
+        )
+        ActionButtonItem(
+            onClick = { /*TODO*/ },
+            icon = Icons.Outlined.Directions,
+            text = "Directions"
+        )
+        ActionButtonItem(
+            onClick = { /*TODO*/ },
+            icon = Icons.Outlined.Share,
+            text = "Share"
+        )
+    }
+}
+
+@Composable
+fun ActionButtonItem(onClick: () -> Unit, icon: ImageVector, text: String) {
+    TextButton(
+        modifier = Modifier.background(
+            color = MaterialTheme.colorScheme.secondaryContainer,
+            shape = RoundedCornerShape(12.dp)
+        ),
+        onClick = { onClick() }
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = "$text Icon"
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        }
+    }
+}
