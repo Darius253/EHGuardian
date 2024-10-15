3e373552b1a978962d55fd4c08d131e717edf582
Darius253 <trondarius13@gmail.com>
2024-10-15 01:12:55 +0100

    Adding image to firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("com.google.firebase:firebase-storage")
     implementation(libs.androidx.runtime.livedata)
     implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
