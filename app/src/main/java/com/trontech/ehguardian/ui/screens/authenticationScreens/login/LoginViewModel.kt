7bb18b732ba8d45f0affa9f18e636063d4cb3fcd
Darius253 <trondarius13@gmail.com>
2024-10-17 19:08:56 +0100

    Renamed project package

--- app/src/main/java/com/trontech/ehguardian/ui/screens/authenticationScreens/login/LoginViewModel.kt
+++ app/src/main/java/com/trontech/ehguardian/ui/screens/authenticationScreens/login/LoginViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.ehguardian.ui.screens.authenticationScreens.login
+package com.trontech.ehguardian.ui.screens.authenticationScreens.login
 
 import android.content.Context
 import android.widget.Toast
@@ -7,7 +7,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.ehguardian.data.repositories.UserRepository
+import com.trontech.ehguardian.data.repositories.UserRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
