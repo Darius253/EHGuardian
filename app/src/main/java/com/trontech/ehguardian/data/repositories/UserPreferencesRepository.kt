b7b4204a5743b1978e28218074ac753de5912b0b
Darius253 <trondarius13@gmail.com>
2024-10-23 23:35:29 +0100

    🆕Worked on PushNotifications

--- app/src/main/java/com/trontech/ehguardian/data/repositories/UserPreferencesRepository.kt
+++ app/src/main/java/com/trontech/ehguardian/data/repositories/UserPreferencesRepository.kt
@@ -1,4 +1,40 @@
 package com.trontech.ehguardian.data.repositories
 
-class UserPreferencesRepository {
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class UserPreferencesRepository(
+    private val dataStore: DataStore<Preferences>
+) {
+    private companion object {
+        val IS_PUSH_NOTIFICATIONS_ENABLED = booleanPreferencesKey("is_push_notifications_enabled")
+    }
+
+    suspend fun savePushNotificationsPreferences(isPushNotificationsEnabled: Boolean) {
+        dataStore.edit{ preferences ->
+            preferences[IS_PUSH_NOTIFICATIONS_ENABLED] = isPushNotificationsEnabled
+
+
+        }
+
+
+    }
+    val isPushNotificationsEnabled: Flow<Boolean> = dataStore.data.catch {
+        if(it is IOException) {
+            Log.e("UserPreferencesRepo", "Error reading preferences.", it)
+            emit(emptyPreferences())
+        } else {
+            throw it
+        }
+    }.map { preferences ->
+        preferences[IS_PUSH_NOTIFICATIONS_ENABLED] ?: false
+    }
 }
