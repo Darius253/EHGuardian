44aa0c2003bea9f290109924dd7a360913287459
Darius253 <trondarius13@gmail.com>
2024-10-27 21:38:13 +0000

    Worked on Notifications

--- app/src/main/java/com/trontech/ehguardian/EhGuardianApplication.kt
+++ app/src/main/java/com/trontech/ehguardian/EhGuardianApplication.kt
@@ -1,32 +1,22 @@
 package com.trontech.ehguardian
 
 import android.app.Application
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import com.trontech.ehguardian.data.repositories.FirebaseUserRepository
 import com.trontech.ehguardian.data.repositories.UserRepository
 import com.trontech.ehguardian.data.services.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.trontech.ehguardian.data.repositories.UserPreferencesRepository
 
 
-private const val PUSHED_NOTIFICATION_PREFERENCE_NAME = "pushed_notification_preferences"
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-    name = PUSHED_NOTIFICATION_PREFERENCE_NAME
-)
+
 
 class EhGuardianApplication : Application() {
 
     lateinit var container: AppContainer
-     lateinit var userPreferencesRepository: UserPreferencesRepository
 
     override fun onCreate() {
         super.onCreate()
         container = AppDataContainer()
-        userPreferencesRepository = UserPreferencesRepository(dataStore)
     }
 }
 
