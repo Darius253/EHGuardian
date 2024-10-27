17f049e81cd2851311465d36da68e844d184c309
Darius253 <trondarius13@gmail.com>
2024-10-27 21:38:33 +0000

    Worked on Notifications

--- app/src/main/java/com/trontech/ehguardian/ui/screens/homeScreens/HomeViewModel.kt
+++ app/src/main/java/com/trontech/ehguardian/ui/screens/homeScreens/HomeViewModel.kt
@@ -12,20 +12,14 @@ import com.trontech.ehguardian.data.models.HospitalItem
 import com.trontech.ehguardian.data.models.MeasurementData
 import com.trontech.ehguardian.data.models.NewsItem
 import com.trontech.ehguardian.data.models.UserModel
-import com.trontech.ehguardian.data.repositories.UserPreferencesRepository
 import com.trontech.ehguardian.data.repositories.UserRepository
-import com.trontech.ehguardian.ui.screens.homeScreens.settings.postNotifications.Notifications
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val userRepository: UserRepository,
-    private val userPreferencesRepo: UserPreferencesRepository,
 ) : ViewModel() {
 
 
@@ -44,11 +38,7 @@ class HomeViewModel(
   private val _isLoading = MutableLiveData(false)
     val isLoading: LiveData<Boolean> = _isLoading
 
-    val pushNotificationsEnabled: StateFlow<Boolean> =
-        userPreferencesRepo.isPushNotificationsEnabled.map{
-            it
 
-        }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
 
 
@@ -175,28 +165,7 @@ class HomeViewModel(
         }
     }
 
-    fun setPushNotifications(context: Context, isEnabled: Boolean) {
-        viewModelScope.launch {
 
-            try {
-                userPreferencesRepo.savePushNotificationsPreferences(isEnabled)
-                if (isEnabled) {
-
-                    val notifications = Notifications(context)
-                    notifications.dailyReminderNotification()
-                }
-                else
-                    Toast.makeText(context, "Post notifications disabled", Toast.LENGTH_SHORT).show()
-            }
-            catch (e: Exception) {
-                _errorMessage.value = e.message
-                Toast.makeText(context, "Failed to enable notifications", Toast.LENGTH_SHORT).show()
-            }
-
-
-        }
-
-    }
 
 
 
