8f94860edf63664e029e73ff5046df6ff0f5977e
Darius253 <trondarius13@gmail.com>
2024-10-31 17:12:29 +0000

    WOrked on scheduling Post Notifications

--- app/src/main/java/com/trontech/ehguardian/backgroundWorkers/PostNotifcationWorker.kt
+++ app/src/main/java/com/trontech/ehguardian/backgroundWorkers/PostNotifcationWorker.kt
@@ -1,4 +1,29 @@
 package com.trontech.ehguardian.backgroundWorkers
 
-class PostNotifcationWorker {
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.trontech.ehguardian.ui.screens.homeScreens.settings.postNotifications.Notifications
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class PostNotificationsWorker(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
+
+
+        return withContext(Dispatchers.IO) {
+            val notifications = Notifications(applicationContext)
+
+            return@withContext try {
+                notifications.dailyReminderNotification()
+                Result.success()
+            } catch (e: Exception) {
+                Result.failure()
+            }
+
+        }
+
+    }
+
+
 }
