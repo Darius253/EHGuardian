cc07add020e12fde6f6d48dd06b6e48408cc1476
Darius253 <trondarius13@gmail.com>
2024-11-06 23:48:11 +0000

    New app Bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.trontech.ehguardian"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
