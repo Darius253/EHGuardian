4801a29e782621fe6a0bef5bf92fae689c4a8ad5
Darius253 <trondarius13@gmail.com>
2024-11-10 22:51:10 +0000

    Renamed Package name

--- app/src/main/java/com/tron/ehguardian/ui/screens/authenticationScreens/ForgotPasswordScreen.kt
+++ app/src/main/java/com/tron/ehguardian/ui/screens/authenticationScreens/ForgotPasswordScreen.kt
@@ -1,4 +1,4 @@
-package com.trontech.ehguardian.ui.screens.authenticationScreens
+package com.tron.ehguardian.ui.screens.authenticationScreens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -39,10 +39,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.trontech.ehguardian.R
-import com.trontech.ehguardian.ui.AppViewModelProvider
-import com.trontech.ehguardian.ui.screens.authenticationScreens.login.LoginViewModel
-import com.trontech.ehguardian.ui.screens.homeScreens.profile.InputField
+import com.tron.ehguardian.R
+import com.tron.ehguardian.ui.AppViewModelProvider
+import com.tron.ehguardian.ui.screens.authenticationScreens.login.LoginViewModel
+import com.tron.ehguardian.ui.screens.homeScreens.profile.InputField
 
 @Composable
 
