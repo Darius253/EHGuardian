51f7782164f9f586f08c8e21cdc071cc60836849
Darius253 <trondarius13@gmail.com>
2024-11-10 22:46:12 +0000

    Renamed Package name

--- app/src/main/java/com/tron/ehguardian/ui/screens/myApp.kt
+++ app/src/main/java/com/tron/ehguardian/ui/screens/myApp.kt
@@ -1,4 +1,4 @@
-package com.trontech.ehguardian.ui.screens
+package com.tron.ehguardian.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -6,7 +6,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.trontech.ehguardian.ui.AppNavigation
+import com.tron.ehguardian.ui.AppNavigation
 
 @Composable
 fun MyApp(
