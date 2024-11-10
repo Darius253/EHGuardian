15675422013b1fa610da4de55b894dfd4dff8ca9
Darius253 <trondarius13@gmail.com>
2024-11-10 22:48:12 +0000

    Renamed Package name

--- app/src/main/java/com/tron/ehguardian/ui/screens/homeScreens/measureScreen/measureScreenComponents.kt
+++ app/src/main/java/com/tron/ehguardian/ui/screens/homeScreens/measureScreen/measureScreenComponents.kt
@@ -1,4 +1,4 @@
-package com.trontech.ehguardian.ui.screens.homeScreens.measureScreen
+package com.tron.ehguardian.ui.screens.homeScreens.measureScreen
 
 
 import androidx.compose.foundation.Image
@@ -37,8 +37,8 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.compose.LottieAnimation
-import com.trontech.ehguardian.R
-import com.trontech.ehguardian.ui.screens.homeScreens.profile.InputField
+import com.tron.ehguardian.R
+import com.tron.ehguardian.ui.screens.homeScreens.profile.InputField
 
 
 @Composable
