39819cdf8bb6fa149d3f80e06ab84927345d84cf
Darius253 <trondarius13@gmail.com>
2024-11-10 22:45:52 +0000

    Renamed Package name

--- app/src/main/java/com/tron/ehguardian/ui/theme/Theme.kt
+++ app/src/main/java/com/tron/ehguardian/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.trontech.ehguardian.ui.theme
+package com.tron.ehguardian.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
